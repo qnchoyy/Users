c409f24f4347d8a4ec2eb214da5a94a026cceff7
qnchoyy <qni191488@abv.bg>
2025-02-05 23:29:20 +0200

    update usercard

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -6,14 +6,27 @@ const UserCard = ({ user }) => {
 
   return (
     <>
-      <div
-        className="bg-white shadow-lg rounded-lg p-4 m-2 w-72 cursor-pointer hover:shadow-xl transition"
+      <section
+        className="w-64 h-60 mx-auto bg-white dark:bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg cursor-pointer"
         onClick={() => setIsModalOpen(true)}
       >
-        <h2 className="text-xl font-bold">{user.name}</h2>
-        <p className="text-gray-600">@{user.username}</p>
-        <p className="text-blue-500">{user.email}</p>
-      </div>
+        <div className="flex items-center justify-between">
+          <span className="text-gray-700 dark:text-gray-400 text-sm">
+            {user.username}
+          </span>
+        </div>
+        <div className="mt-8">
+          <h2 className="text-gray-900 dark:text-white font-bold text-2xl tracking-wide">
+            {user.name}
+          </h2>
+        </div>
+        <p className="text-blue-600 dark:text-emerald-400 font-semibold mt-2.5">
+          {user.email}
+        </p>
+        <p className="text-blue-600 dark:text-emerald-400 font-semibold mt-2.5">
+          Street: {user.address.street}
+        </p>
+      </section>
 
       {isModalOpen && (
         <UserModal user={user} closeModal={() => setIsModalOpen(false)} />
