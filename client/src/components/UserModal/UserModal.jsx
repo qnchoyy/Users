6e1c8ef5fd471f4dac5154588cfbf26b8c47624a
qnchoyy <qni191488@abv.bg>
2025-02-05 23:29:37 +0200

    update UserModal

--- client/src/components/UserModal/UserModal.jsx
+++ client/src/components/UserModal/UserModal.jsx
@@ -4,7 +4,7 @@ const UserModal = ({ user, closeModal }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-80">
-        <h2 className="text-2xl font-bold">{user.name}</h2>
+        <h2 className="text-2xl text-black font-bold">{user.name}</h2>
         <p className="text-gray-500">@{user.username}</p>
         <p className="text-gray-700">📧 {user.email}</p>
         <p className="text-gray-700">📞 {user.phone}</p>
