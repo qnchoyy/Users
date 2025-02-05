e9f7ba254b6ceca05f726798ecc0b00917b94ff2
qnchoyy <qni191488@abv.bg>
2025-02-05 23:29:48 +0200

    update App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import UserList from "./components/UserList/UserList";
+import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
+import HeroSection from "./components/Hero/Hero";
+import Header from "./components/Header/Header";
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">User Profiles</h1>
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
+      <Header />
+      <HeroSection searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      {/* <div className="flex justify-center relative mb-6">
+        <h1 className="text-3xl font-bold">User Profiles</h1>
+        <div className="absolute right-0">
+          <ThemeToggle />
+        </div>
+      </div> */}
 
-      <div className="flex justify-center mb-6">
+      {/* <div className="flex justify-center mb-6">
         <input
           type="text"
-          placeholder="Търси по име или псевдоним..."
+          placeholder="Search by name or nickname..."
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          className="p-2 border rounded-lg w-80"
+          className="p-2 border dark:border-gray-600 dark:bg-gray-800 dark:text-white rounded-lg w-80 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-      </div>
+      </div> */}
 
-      <UserList searchQuery={searchQuery} />
+      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <UserList searchQuery={searchQuery} />
+      </div>
     </div>
   );
 };
